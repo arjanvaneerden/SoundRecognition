00d8cfb44ffceae71ad141818148ad0a298d29e7
romusters <romusters@gmail.com>
2015-05-07 12:48:19 +0200

    Min-max scaling added.

--- src/svm/svm.py
+++ src/svm/svm.py
@@ -1,10 +1,29 @@
 from sklearn import svm
 import numpy as np
+from sklearn import preprocessing.MinMaxScaler as scale
+
 class SVM:
 	def __init__(self):
 		print "Haha! Een SVM!"
 
-	def getFeatures(self, array): 
+	def getCochleagram(self):
+		#FIXME read cochleagram files
+		return
+
+	def getAnnotation(self):
+		#FIXME read annotation files
+		return 
+	
+	def scaleData(self, data, (x, y)):
+		scale = scale((x, y))
+		return scale.fit_transform(data)
+
+
+	def optimize():
+
+
+	def getFeatures(self, array):
+		#FIXME multi channel support 
 		#sum over column
 		column = np.sum(array, 0)
 		#sum over rows
