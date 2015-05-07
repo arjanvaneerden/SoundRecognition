26156519effd6c9d4ece5331dba243a475229f34
romusters <romusters@gmail.com>
2015-05-07 12:21:28 +0200

    Projection features added.

--- src/svm/svm.py
+++ src/svm/svm.py
@@ -1,7 +1,12 @@
 from sklearn import svm
-
+import numpy as np
 class SVM:
 	def __init__(self):
 		print "Haha! Een SVM!"
 
-		
+	def getFeatures(self, array): 
+		#sum over column
+		column = np.sum(array, 0)
+		#sum over rows
+		row = np.sum(array, 1)
+		return (column, row)
