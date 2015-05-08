9694af8b43893d671fb24f7d04504d53bdc01d7f
AntonMulder <anton@AntonMulder.(none)>
2015-05-08 10:37:36 +0200

    SVM

--- src/svm/svm.py
+++ src/svm/svm.py
@@ -1,10 +1,92 @@
 from sklearn import svm
 import numpy as np
-#from sklearn import preprocessing.MinMaxScaler as scale
+import cPickle as pickle
 
 class SVM:
-	def __init__(self):
-		print "Haha! Een SVM!"
+	def __init__(self, C = 1.0, kernel = "rbf", gamma = 0.0, coef0 = 0.0, probability = True, tol = 0.001, verbose = False):
+		self.clf = svm.SVC(C = C, kernel = kernel, gamma = gamma, coef0 = coef0, probability = probability, tol = tol)
+
+
+	def train(self, features, targets):
+		self.clf.fit(features, targets)
+
+
+	'''
+		Returns predicted class with probability.
+
+		Format:
+			(class, probability)
+	'''
+	def predict(self, feature):
+		print (self.clf.predict(feature)[0], np.amax(self.clf.predict_proba(feature)) )
+
+
+	'''
+		Save SVM to file
+	'''
+	def save(self, name):
+		try:
+			pickle.dump(self.clf, open(name, "wb"))
+		except:
+			print "Could not save SVM"
+			return False
+
+		return True
+
+	def load(self, name):
+		try:
+			self.clf = pickle.load(open(name, "rb"))
+		except:
+			print "Could not load SVM"
+			return False
+		return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	def getCochleagram(self):
 		#FIXME read cochleagram files
